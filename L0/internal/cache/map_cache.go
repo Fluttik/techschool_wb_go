e4375e22f9d5ed0fff6ebd18ff8e07431181f65c
Николай Королёв <korolevns@tecon.ru>
2025-11-07 14:39:04 +0300

    Добавил блокировку при обращении к кешу

--- L0/internal/cache/map_cache.go
+++ L0/internal/cache/map_cache.go
@@ -2,13 +2,21 @@ package cache
 
 import (
 	"log"
+	"sync"
 
 	"gorm.io/gorm"
 
 	"L0/internal/db"
 )
 
-var orders = make(map[string]*db.Order)
+type Cache struct {
+	mu     sync.RWMutex
+	orders map[string]*db.Order
+}
+
+var cache = &Cache{
+	orders: make(map[string]*db.Order),
+}
 
 func InitCache(dbConn *gorm.DB) {
 	// получаем послдние 100 заказов из БД для сохранения в кеше
@@ -17,30 +25,38 @@ func InitCache(dbConn *gorm.DB) {
 		log.Println("Не удалось загрузить заказы из БД.")
 	}
 
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
 	// заполнение кеша
 	for i := range dbOrders {
 		order := &dbOrders[i]
-		orders[order.OrderUID] = order
+		cache.orders[order.OrderUID] = order
 		log.Printf("Заказ с id %s загружен в кеш", order.OrderUID)
 	}
 
-	if len(orders) == 0 {
+	if len(cache.orders) == 0 {
 		log.Println("Кеш пуст — в БД пока нет заказов")
 	} else {
-		log.Printf("В кеш загружено %d заказа(ов)\n", len(orders))
+		log.Printf("В кеш загружено %d заказа(ов)\n", len(cache.orders))
 	}
 }
 
 // Функкия для получения 1 заказа по его id
 func GetOrderByID(id string) (*db.Order, bool) {
-	order, exists := orders[id]
+	cache.mu.RLock()
+	defer cache.mu.RUnlock()
+	order, exists := cache.orders[id]
 	return order, exists
 }
 
 // Функия для получения всех заказов (использовалась для тестов, в проекте не используется)
 func GetOrders() []*db.Order {
+	cache.mu.RLock()
+	defer cache.mu.RUnlock()
+
 	var result []*db.Order
-	for _, order := range orders {
+	for _, order := range cache.orders {
 		result = append(result, order)
 	}
 	return result
@@ -48,5 +64,7 @@ func GetOrders() []*db.Order {
 
 // Функция для добавления заказа в кеш
 func SetOrder(order *db.Order) {
-	orders[order.OrderUID] = order
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+	cache.orders[order.OrderUID] = order
 }
